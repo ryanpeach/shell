9c158a7b1390408888d3d210b229c2c2316d6873
Ryan Peach <rgpeach10@gmail.com>
2024-08-05 19:11:31 +0000

    Should be working, but don't currently see any results in rust

--- home/.config/nvim/lua/plugins/trouble.lua
+++ home/.config/nvim/lua/plugins/trouble.lua
@@ -1,6 +1,6 @@
 return {
     "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    opts = {auto_open = true, auto_close = true}, -- for default options, refer to the configuration section for custom setup.
     cmd = "Trouble",
     keys = {
         {
