997511388765c7df537a8026c97067880369f52f
Ryan Peach <rgpeach10@gmail.com>
2024-08-21 19:22:01 +0000

    Added jq plugin for vim json lookup

---
+++ home/.config/nvim/lua/jq.lua
@@ -0,0 +1,9 @@
+return {
+    "vito-c/jq.vim",
+    lazy = true, -- Lazy load the plugin
+    cmd = {"Jq"}, -- Only load when the Jq command is called
+    config = function()
+        -- You can add additional configuration here if needed
+    end
+}
+
