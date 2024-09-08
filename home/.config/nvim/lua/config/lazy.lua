998276a45eaccd51a105bb56c88e194bbdbf5a90
Ryan Peach <rgpeach10@gmail.com>
2024-09-08 19:03:04 -0400

    pre commit

--- home/.config/nvim/lua/config/lazy.lua
+++ home/.config/nvim/lua/config/lazy.lua
@@ -49,7 +49,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
-    
+
     -- import/override with your plugins
     { import = "plugins" },
   },
