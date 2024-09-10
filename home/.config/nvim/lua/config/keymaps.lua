e7436feba11ca260fdc0ed124a251d70641a7688
Ryan Peach <rgpeach10@gmail.com>
2024-09-09 21:58:19 -0400

    lua fmt

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -1,11 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
 local wk = require("which-key")
 
 -- Override only <leader><tab>h and <leader><tab>l for tab navigation
 wk.add({
-    { "<leader><tab>h", "<cmd>tabprevious<cr>", desc="Previous Tab" },
-    { "<leader><tab>l", "<cmd>tabnext<cr>", desc="Next Tab" },
+    {"<leader><tab>h", "<cmd>tabprevious<cr>", desc = "Previous Tab"},
+    {"<leader><tab>l", "<cmd>tabnext<cr>", desc = "Next Tab"}
 })
